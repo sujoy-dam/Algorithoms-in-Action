d696e54b6501da9f2129b7e01bf5fcf1c7212068
sujoy-dam <sujooy01@gmail.com>
2025-11-06 07:47:58 +0600

    second commit

--- basic-cach-with-map.js
+++ basic-cach-with-map.js
@@ -1,15 +1,29 @@
-const expensiveTask = (id)=>{
-    console.log("Ran the expensive task for:", id);
+const datCache = new Map()
 
-    return {
-        id: id,
-        data:`Some data for id: ${id}`,
-        timestamp: new Date().getTime()
-    }
+const expensiveTask = (id)=>{
+console.log("Ran the expensive task for:", id)
+
+return {
+    id: id,
+    data:`Some data for id: ${id}`,
+    timestamp: new Date().getTime()
 }
+}
+
 const getData= (id)=>{
-    const x = expensiveTask(id)
-    console.log(x)
-    return x
+
+    if(datCache.has(id)){
+        console.log("cachee hit for id:", id)
+        return datCache.get(id)
+    }
+    console.log("Cash Miss for id :", id)
+    const data = expensiveTask(123)
+    datCache.set(id, data)
+    return data;
+
 }
+
+console.log(datCache)
 console.log(getData(123))
+console.log(getData(123))
+console.log(datCache)
